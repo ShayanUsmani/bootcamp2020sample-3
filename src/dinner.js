7834ccc3cfe72a39f4349b3e2a18a5a162f49b12
Shayan Usmani <shayan.usmani16@gmail.com>
2020-10-08 00:10:57 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Dinner(props){
     return(
-        <h1>Today we are serving {props.dishName}</h1>
+        <h1>Tonight we are serving {props.dishName}</h1>
     )
 
 }
